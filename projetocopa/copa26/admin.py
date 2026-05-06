fb0020144af2a16ba87af5dbc75d706a2d2288d0
Joao Gabriel Silva Gama <20242014040015@ifrn.local>
2026-05-06 15:40:53 -0300

    chore: criacao do requirements

--- projetocopa/copa26/admin.py
+++ projetocopa/copa26/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Tecnico)
+admin.site.register(Selecao)
+admin.site.register(Jogo)
+admin.site.register(EventoJogo)
+admin.site.register(Jogador)
